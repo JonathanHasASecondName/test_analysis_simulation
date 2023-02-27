60598735ec292015cb6186e6785b69eb391b73f3
jonathan <jonathanpoot3009@gmail.com>
2023-02-27 10:55:56 +0100

    Modified spectrogram

--- matplot_lib_spectrogram.py
+++ matplot_lib_spectrogram.py
@@ -1,20 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-sr = 2000
+sample_time = 1000
+sr = 2048
 # sampling interval
 ts = 1.0/sr
-t = np.arange(0,1,ts)
-NFFT = 1024
+t = np.arange(0,sample_time,ts)
+NFFT = 1024*16
 
-freq = 5
-scaling_factor = 10  # allows more detailed transforms in frequency
-x = 3*np.cos(2*np.pi*freq*t*scaling_factor)
-x[:1000] = 3*np.cos(2*np.pi*freq*t[:1000]*scaling_factor)
-x[1000:] = 3*np.cos(2*np.pi*freq*2*t[1000:]*scaling_factor)
+freq = 25
+x = 3*np.cos(2*np.pi*freq*t)
+x[int(len(t)/2):] = 3*np.cos(2*np.pi*freq*2*t[int(len(t)/2):])
 
 fig, (ax1, ax2) = plt.subplots(nrows=2)
 ax1.plot(t, x)
-Pxx, freqs, bins, im = ax2.specgram(x, NFFT=NFFT, noverlap=1, Fs=sr/scaling_factor)
-plt.axis(ymin=0, ymax=10)
+Pxx, freqs, bins, im = ax2.specgram(x, NFFT=NFFT, Fs=sr)
+plt.axis(ymin=0, ymax=100)
 plt.show()
