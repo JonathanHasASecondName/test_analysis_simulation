1384505523945dac21e4b99244bf640248115ab5
apelin <a.m.pelin-1@student.tudelft.nl>
2023-03-13 11:39:34 +0100

    added data - sorry oopsie

--- GPS.py
+++ GPS.py
@@ -1,3 +1,28 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_mic_array():
+    darray = np.loadtxt("data/config.txt")
+
+    x = darray[:, -2]
+    y = darray[:, -1]
+
+    plt.scatter(x,y)
+    plt.show()
+    return
+
+def plot_height_drone():
+    dp = pd.read_csv("data/GPS_D1F1.csv")
+
+    y = dp.iloc[:, 3]
+    x = dp.iloc[:, 0]
+
+    plt.plot(x,y)
+    plt.xlabel("Time(ms)")
+    plt.ylabel("Drone altitude(m)")
+    plt.show()
+    return
+
+plot_height_drone()
 
