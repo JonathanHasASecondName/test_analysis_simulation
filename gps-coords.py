058a8388adc2f84097d7fa7eb7955e5812296c80
Parashoo <adam.dima@gmail.com>
2023-03-13 11:27:55 +0100

    Wrote GPS coordinate conversion helper functions

--- gps-coords.py
+++ gps-coords.py
@@ -1,10 +1,10 @@
-from math import sin, cos, sqrt
+from math import sin, cos, sqrt, atan2
 from math import pi, radians
 
 array_center_gps = [51.51878, 5.85821]
 radius = 6371000 # Stolen from some random website: https://planetcalc.com/7721/
 
-def shitcum(coords):
+def equirectangular(coords):
     acg = [radians(array_center_gps[0]), radians(array_center_gps[1])]
     coords = [radians(coords[0]), radians(coords[1])]
     dlat = coords[0]-acg[0]
@@ -16,11 +16,21 @@ def shitcum(coords):
     converted = [-xg * cos(pi/6) - yg * sin(pi/6), xg * sin(pi/6) - yg * cos(pi/6)]
     return [xg, yg]
 
-def main():
-    test_coords = [51.52, 5.9]
-    transformed = shitcum(test_coords)
-    print(sqrt(transformed[0]**2 + transformed[1]**2))
+def haversine(coords):
+    phi1, phi2 = radians(array_center_gps[0]), radians(coords[0])
+    dphi = phi2 - phi1
+    dlambda = radians(coords[1]-array_center_gps[1])
+    
+    a = sin(dphi/2)**2 + cos(phi1)*cos(phi2)*(sin(dlambda/2))**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    d = radius * c # Distance to array 
 
-if __name__=="__main__":
-    main()
+    y = sin(dlambda) * cos(phi2)
+    x = cos(phi1)*sin(phi2)-sin(phi1)*cos(phi2)*cos(dlambda)
+    theta = atan2(y, x) # Bearing to array
+
+    reltheta = pi + (theta + pi/6)
+    x, y = d * cos(reltheta), d*sin(reltheta) # Relative coordinates
+
+    return (d, theta), (x, y) 
 
