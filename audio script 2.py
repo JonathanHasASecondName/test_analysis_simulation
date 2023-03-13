aa27b55f7c1984a03dcd176e55af2831243af30a
jonathan <jonathanpoot3009@gmail.com>
2023-03-13 09:23:31 +0100

    Modified spectrogram

---
+++ audio script 2.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.io import wavfile
+from scipy import signal
+
+# Define function to read CSV file
+def read_csv(filename):
+    df = pd.read_csv(filename, header=None)
+    return df.to_numpy()
+
+# Define function to preprocess data
+def preprocess_data(data):
+    data = np.squeeze(data)
+    data = np.asarray(data, dtype=np.float32)
+    data = np.nan_to_num(data)
+    return data
+
+# Define function to generate spectrogram data
+def generate_spectrogram_data(data, sampling_rate):
+    f, t, spectrogram_data = signal.spectrogram(data, fs=sampling_rate, nperseg=1024, noverlap=512, scaling='spectrum')
+    spectrogram_data = 20*np.log10(np.abs(spectrogram_data) + 1e-10)
+    return f, t, spectrogram_data
+
+# Ask user for input files
+main_file = input("Enter the filename for the main data: ")
+noise_file = input("Enter the filename for the background noise: ")
+subtract_noise = input("Do you want to subtract the background noise? (y/n): ")
+
+# Read data from files
+main_data = read_csv(main_file)
+noise_data = read_csv(noise_file)
+
+# Preprocess data
+main_data = preprocess_data(main_data)
+noise_data = preprocess_data(noise_data)
+
+# Compute spectrogram data for noise
+f_noise, t_noise, spectrogram_noise = generate_spectrogram_data(noise_data, 50000)
+
+# Compute average noise level
+avg_noise_level = np.mean(spectrogram_noise[:, :], axis=1)
+
+# Compute spectrogram data for main data
+f_main, t_main, spectrogram_main = generate_spectrogram_data(main_data, 50000)
+
+# Subtract noise from main data
+if subtract_noise.lower() == 'y':
+    spectrogram_main = spectrogram_main - avg_noise_level.reshape(-1, 1)
+
+# Crop spectrogram data
+spectrogram_noise = spectrogram_noise[:125, :]
+spectrogram_main = spectrogram_main[:125, :]
+
+# Plot spectrogram
+fig, axs = plt.subplots(1, 2, figsize=(16, 8))
+if subtract_noise.lower() == 'y':
+    axs[0].pcolormesh(t_main, f_main[:125], spectrogram_main, cmap='viridis', shading='auto')
+    axs[0].set_title('Main Data (Noise-Free)')
+else:
+    axs[0].pcolormesh(t_noise, f_noise[:125], spectrogram_noise, cmap='viridis', shading='auto')
+    axs[0].set_title('Background Noise')
+axs[0].set_xlabel('Time (s)')
+axs[0].set_ylabel('Frequency (Hz)')
+axs[1].pcolormesh(t_main, f_main[:125], spectrogram_main, cmap='viridis', shading='auto')
+axs[1].set_title('Main Data')
+axs[1].set_xlabel('Time (s)')
+axs[1].set_ylabel('Frequency (Hz)')
+plt.show()
---
+++ audio script.py
@@ -0,0 +1,62 @@
+import csv
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the main file
+with open('main_file.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = np.array([[float(val) for val in row if val != ''] for row in reader])
+
+# Load the background noise file and calculate its average
+subtract_noise = input("Do you want to subtract background noise? (y/n): ").lower() == "y"
+if subtract_noise:
+    with open('background_noise.csv', newline='') as f:
+        reader = csv.reader(f)
+        noise_data = np.array([[float(val) for val in row if val != ''] for row in reader])
+        noise_avg = np.mean(noise_data, axis=0)
+
+        # Subtract the noise average from the main file
+        clean_data = data - noise_avg
+else:
+    clean_data = data
+
+# Normalize the data
+max_val = np.abs(clean_data).max()
+if max_val > 1.0:
+    clean_data /= max_val
+
+# Set the audio parameters
+num_channels = 1
+sample_rate = 50000
+
+# Write the audio files
+with wave.open('original_audio.wav', 'w') as file:
+    file.setparams((num_channels, 3, sample_rate, 0, 'NONE', 'Uncompressed'))
+    file.writeframes(data.tobytes())
+
+with wave.open('clean_audio.wav', 'w') as file:
+    file.setparams((num_channels, 3, sample_rate, 0, 'NONE', 'Uncompressed'))
+    file.writeframes(clean_data.tobytes())
+
+# Compute and plot the spectrograms
+plt.figure(figsize=(12, 6))
+
+# Original spectrogram
+plt.subplot(1, 2, 1)
+plt.specgram(data.flatten(), NFFT=4096*128, Fs=sample_rate)
+plt.ylim(1200, 1500)
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
+plt.title('Original Spectrogram')
+
+# Cleaned spectrogram
+plt.subplot(1, 2, 2)
+plt.specgram(clean_data.flatten(),NFFT=4096*128,  Fs=sample_rate)
+plt.ylim(1200, 1500)
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
+plt.title('Cleaned Spectrogram')
+
+plt.tight_layout()
+plt.show()
