b22f137942c500613474f3ed807bd56e6d13e9db
germenfer <germenfer03@gmail.com>
2023-03-16 10:00:30 +0100

    :D

--- the_real_audio_script.py
+++ the_real_audio_script.py
@@ -1,8 +1,17 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
 
-
-with open('Array_D1F1.csv', 'r') as f:
+with open("data/Array_D1F1.csv", 'r') as f:
     data = list(csv.reader(f, delimiter=","))
 
-data = np.array(data)
+data = np.array(data, float)
+data = data[0,:]
+data = data[:len(data)-1]
+
+t = np.arange(0,15,1/50000)
+
+plt.plot(t,data,"b")
+plt.show()
+
+print(len(data))
