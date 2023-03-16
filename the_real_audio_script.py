30724b8016af9e312a8ace10abb65e56e4cc4d4c
germenfer <germenfer03@gmail.com>
2023-03-16 11:05:49 +0100

    :D

--- the_real_audio_script.py
+++ the_real_audio_script.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csv
+import scipy.signal as signal
+import scipy.io.wavfile as wavfile
 import matplotlib.pyplot as plt
 import soundfile as sf
 
@@ -8,7 +10,7 @@ with open("data/Array_D1F1.csv", 'r') as f:
 
 data = np.array(data, float)
 data = data[0,:]
-data = data[:len(data)-1]
+data = data[:len(data)-1]*10000
 
 print("Data OK")
 
@@ -16,13 +18,32 @@ t = np.arange(0,15,1/50000)
 
 print("Time OK")
 
-plt.plot(t,data,"b")
+# compute sample rate, assuming times are in seconds
+sample_rate_hz = 50000
+sf.write('recording.wav', data, sample_rate_hz)
+print("Output OK")
+
+# load audio file
+sample_rate, data = wavfile.read('recording.wav')
+
+# create spectrogram
+frequencies, times, spectrogram = signal.spectrogram(data, fs=sample_rate)
+
+# plot spectrogram
+plt.subplot(2,1,1)
+plt.title("Pressures")
+plt.plot(t,data,"b",linewidth=0.05)
+plt.ylabel('Pressure [Pa]')
+plt.xlabel('Time [sec]')
+
+plt.subplot(2,1,2)
+plt.title("Spectrogram")
+plt.ylim([0,1000])
+plt.pcolormesh(times, frequencies, spectrogram)
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.tight_layout()
 plt.show()
 
 print("Plot OK")
 
-# compute sample rate, assuming times are in seconds
-sample_rate_hz = 50000
-sf.write('recording.wav', data, sample_rate_hz)
-
-print("Output OK")
