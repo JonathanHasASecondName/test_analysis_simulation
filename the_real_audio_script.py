6e95cb6036c5e1b0338505eefa67c8f7b9e934a4
alicegirardello <alice.girardello.13@gmail.com>
2023-03-16 10:01:48 +0100

    hello

--- the_real_audio_script.py
+++ the_real_audio_script.py
@@ -1,6 +1,8 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+import pandas as pd
+import soundfile as sf
 
 with open("data/Array_D1F1.csv", 'r') as f:
     data = list(csv.reader(f, delimiter=","))
@@ -13,5 +15,18 @@ t = np.arange(0,15,1/50000)
 
 plt.plot(t,data,"b")
 plt.show()
+
+# assume we have columns 'time' and 'value'
+df = pd.read_csv('recording.csv')
+
+# compute sample rate, assuming times are in seconds
+times = df['time'].values
+n_measurements = len(times)
+timespan_seconds = times[-1] - times[0]
+sample_rate_hz = int(n_measurements / timespan_seconds)
+
+# write data
+data = df['value'].values
+sf.write('recording.wav', data, sample_rate_hz)
 
 print(len(data))
