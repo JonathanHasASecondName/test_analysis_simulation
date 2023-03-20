641090a13331eeb3f7899b4f142128f8adfa6716
jonathan <jonathanpoot3009@gmail.com>
2023-03-20 09:03:52 +0100

    added files

--- audio_script_3.py
+++ audio_script_3.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import signal
 
-n_perseg = 1024*16
+n_perseg = 1024*8*2
 n_frequencies = 4096
 
 def read_csv(filename):
@@ -22,20 +22,21 @@ def preprocess_data(data):
     data = np.nan_to_num(data)
     return data
 
-main_file = "Data/Drone4_Flight1/Array_D4F1.csv"
-noise_file = "Data/Background/Array_Background.csv"
+main_file = "data/Drone4_Flight1/Array_D4F1.csv"
+# noise_file = "Data/Background/Array_Background.csv"
 subtract_noise = 'n'
 # Read data from files
 main_data = read_csv(main_file)
-noise_data = read_csv(noise_file)
+# noise_data = read_csv(noise_file)
 main_data = preprocess_data(main_data)
 
-noise_data = preprocess_data(noise_data)
+# noise_data = preprocess_data(noise_data)
 # take microphone 16
-noise_data = noise_data[:, 16][:main_data.shape[0]]
+# noise_data = noise_data[:, 16][:main_data.shape[0]]
 
 f, t, Sxx = signal.spectrogram(main_data, fs=50000, nperseg=n_perseg)
 plt.pcolormesh(t, f[:n_frequencies], Sxx[:n_frequencies, :], cmap='plasma')
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
+plt.axis(ymin=0, ymax=1000)
 plt.show()
